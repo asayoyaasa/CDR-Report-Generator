1640c5541cc961d6c202bc31a28ebbd7417cf026
Tiga asa yosa <asacrypto@post.com>
2023-11-20 06:12:27 +0700

    Update 8.0. run this one.py

--- 8.0. run this one.py
+++ 8.0. run this one.py
@@ -19,121 +19,25 @@ output_file_vapro = 'xvapro.csv'
 # Check if 3cx.csv exists
 if not os.path.exists(file_path_3cx):
     # If 3cx.csv does not exist, run the 3cx_combiner.py script
-    subprocess.run(["python", "3cx_combiner.py"])
+    subprocess.run(["python", "8.1. 3cx_combiner.py"])
     print("3cx_combiner.py script executed")
 
 # Mapping for 3CX
 caller_id_mapping = {
-    "ARTHUR GARCIA 2 FANVIL-X4 (112)": "ARTHUR GARCIA",
-    "GEORGE KRUGER HUNTERS (115)": "MATT GUNNER",
-    "JACKSON FURY HUNTERS (116)": "JACKSON FURY",
-    "JACOB FOX 2 FANVIL X4U (114)": "JACOB FOX",
-    "JOHN D'SOUZA (102)": "JOHN D'SOUZA",
-    "LEWIS NAVARO HUNTERS (124)": "LEWIS NAVARO",
-    "MICHAEL HANSON (106)": "JASON SPARK",
-    "SOFIA KIM (104)": "SOFIA KIM"
+    
     # Add other mappings as needed
 }
 
 # Mapping for PBX. For clarity this is #0001<<extension number 
 cnam_mapping = {
-    "DOMINIC HUNT":    "DOMINIC HUNT",           #0001
-    "JACOB FOX":       "JACOB FOX",              #0002
-    "ARTHUR GARCIA":   "ARTHUR GARCIA",          #0003
-    "MIA HUNT":        "MIA HUNT",               #0004
-    "EDGAR LOU":       "EDGAR LOU",              #0005
-    "CAROL TAN":       "CAROL TAN",              #0006
-    "MATT GUNNER":     "MATT GUNNER",            #0007
-    "JACKSON FURY":    "JACKSON FURY",           #0008
-    "LEWIS NAVARO":    "LEWIS NAVARO",           #0009
-    "JOHN D'SOUZA":    "JOHN D'SOUZA",           #0010
-    "KATE DHILLON":    "NO USER KATE DHILLON",   #0011
-    "BRYAN LIEM":      "NO USER BRYAN LIEM",     #0012
-    "IT SUPPORT - EDDY": "IT SUPPORT - EDDY",    #0013
-    "ALLAN LOU":       "NO USER ALLAN LOU",      #0014
-    "JASON SPARK":     "JASON SPARK",            #0015
-    "CAROLINE ROSE":   "CAROL LYNN ROSE",        #0021
-    "SOFIA KIM":       "SOFIA KIM",              #0022
-    "ANNA YOUNG":      "ANNA YOUNG",             #0023
-    "OLIVER WILLIAMS": "OLIVER WILLIAMS",        #0024
-    "EMILY PARK":      "EMILY PARK",             #0025
-    "NATHAN LEE":      "LOGAN PARK",             #0026
-    "USER 27":         "NO USER USER 27",        #0027
-    "JAIMIE LYNN":     "JAIMIE LYNN",            #0031
-    "KYLIE PARK":      "NO USER KYLIE PARK",     #0032
-    "HANNA KIM":       "NO USER HANNA KIM",      #0033
-    "DANZEL WEI":      "DANNY LIM",              #0034
-    "IT - NANDA":      "NO USER IT - NANDA",     #0035
-    "ALEX TAN":        "GIA WATTS",              #0036
-    "CHARLIE TAN":     "NO USER CHARLIE TAN",    #0037
-    "ROGER LEE":       "ROGER LEE",              #0038
-    "USER-0039":       "NO USER USER-0039",      #0039
-    "WILLIAM LEE":     "WILLIAM LEE",            #0040
-    "DANIEL TAN":      "NO USER DANIEL TAN",     #0041
-    "JAMES SILVA":     "JAMES SILVA",            #0042
-    "IT SUPPORT":      "IT SUPPORT",             #0666
-    "777":             "777",                    #0777
-    "asa":             "asa",                    #1234
-    "Mia":             "Mia",                    #9999
-
-
+    
     # Add other mappings as needed
 }
 
 # Mapping for VAPRO
 number_to_name = {
-    "0064*008": "ANNA YOUNG",
-    "0064*777": "ASA",
-    "0064*555": "DATAMINER",
-    "0064*669": "HENDRA",
-    "0064*123": "ADNAN",
-    "0064*170": "NANDA",
-    "0064*333": "IT1",
-    "0064*323": "IT2",
-    "0064*666": "IT3",
-    "0064*667": "IT4",
-    "0064*203": "JAMES SILVA",
-    "0064*004": "CHARLIE TAN",
-    "0064*202": "ROGER LEE",
-    "0064*006": "CAROL LYNN ROSE",
-    "0064*052": "HANNA KIM",
-    "0064*103": "JACOB FOX",
-    "0064*990": "MIA HUNT",
-    "0064*012": "NEW TQ 012",
-    "0064*665": "NEW TQ 665",
-    "0064*013": "EMILY PARK",
-    "0064*077": "NEW TQ 077",
-    "0064*108": "GIA WATTS",
-    "0064*106": "ALLAN LOU",
-    "0064*017": "ARTHUR GARCIA",
-    "0064*002": "CAROL TAN",
-    "0064*015": "JASON SPARK",
-    "0064*001": "DOMINIC HUNT",
-    "0064*003": "MATT GUNNER",
-    "0064*104": "JACKSON FURY",
-    "0064*109": "JOHN D'SOUZA",
-    "0064*018": "GIA WATTS",
-    "0064*051": "LEWIS NAVARO",
-    "0064*991": "MIA HUNT",
-    "0064*010": "LOGAN PARK",
-    "0064*066": "SOFIA KIM",
-    "0064*005": "ANNA YOUNG",
-    "0064*057": "BRYAN LIEM",
-    "0064*014": "DANNY LIM",
-    "0064*007": "EDGAR LOU",
-    "0064*055": "JAIMIE LYNN",
-    "0064*053": "OLIVER WILLIAMS",
-    "0064*105": "KYLIE PARK",
-    "0064*222": "LUCY WOO",
-    "0064*700": "MR.RANDEE",
-    "0064*668": "randee",
-    "0064*701": "RANDEE BP",
-    "0064*686": "Randee TEst",
-    "0064*201": "TRENING 1",
-    "0064*204": "TRENING 3",
-    "0064*205": "WILLIAM LEE",
-    "0064*206": "TRENING 5",
-    "0064*999": "USER - 999",
+    
+     # Add other mappings as needed
 }
 
 # Check if 3cx.csv exists
@@ -197,6 +101,6 @@ if os.path.exists(file_path_3cx):
     print("VAPRO REPORT DONE")
 
 # Run the combined.py script
-    subprocess.run(["python", "v14.1.py"])
-    print("v14.1.py script executed")
+    subprocess.run(["python", "8.2. final_combiner.py"])
+    print("final_combiner.py script executed")
 
